b5a4ac6f7ebc56cb94a6c062e38f0056138c2c5a
epitstickpan <40240392+epitstickpan@users.noreply.github.com>
2023-02-28 12:49:21 -0500

    Improve handling of new run

--- run_once_install_rust.sh
+++ run_once_install_rust.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path -y
+if [[ -f $HOME/.cargo/env ]]; then
+  source $HOME/.cargo/env
+  rustup self update
+  rustup update
+ else
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path -y
+  source $HOME/.cargo/env
+fi
 
-cargo install starship exa ripgrep fd-find zoxide
+cargo install starship exa ripgrep fd-find zoxide cargo-update topgrade
 
